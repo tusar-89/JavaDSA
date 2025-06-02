5b66cd598cb16d9262e9258cfc5eea47b517264f
tusar-89 <sahut8941@gmail.com>
2025-06-02 03:05:18 -0900

    Create 34-->Zeros count.java

---
+++ Dsa Practice/34-->Zeros count.java
@@ -0,0 +1,25 @@
+Problem-->
+
+Given an array arr of only 0's and 1's. 
+The array is sorted in such a manner that all the 1's are placed first and then they are followed by all the 0's.
+Find the count of all the 0's.
+  
+  https://www.geeksforgeeks.org/problems/count-the-zeros2550--141153/1?page=19&category=Arrays&difficulty=Basic,Easy&status=unsolved&sortBy=submissions
+
+Ans-->
+
+  
+class Solution {
+    int countZeroes(int[] arr) {
+        // code here
+        int count=0;
+        for(int ele:arr)
+        {
+            if(ele==0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
