33b69e60987608b4b1dd16aa01b073b89a015031
tusar-89 <sahut8941@gmail.com>
2025-07-31 05:12:48 -0900

    Create 91-->Prime Number.java

---
+++ Dsa Practice/91-->Prime Number.java
@@ -0,0 +1,25 @@
+/*
+Problem-->
+
+Given a number n, determine whether it is a prime number or not.
+Note: A prime number is a number greater than 1 that has no positive divisors other than 1 and itself.
+
+https://www.geeksforgeeks.org/problems/prime-number2314/1?page=2&status=unsolved&sortBy=submissions
+
+Ans-->
+  */
+
+class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        if (n <= 1) return false; // 0 and 1 are not prime numbers
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
