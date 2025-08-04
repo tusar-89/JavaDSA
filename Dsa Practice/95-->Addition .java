678bd210cb75b672ca0ec2ef1ffcc6cf6108307f
tusar-89 <sahut8941@gmail.com>
2025-08-04 08:44:33 -0900

    Create 95-->Addition .java

---
+++ Dsa Practice/95-->Addition .java
@@ -0,0 +1,24 @@
+/*
+Problem-->
+
+  Design a class as described.
+class: Addition
+function: function name: add, parameters: a(int),
+b(int), return type: int
+access specifier: public, static: yes
+task: returns the sum of the values given in the parameter.
+
+  https://www.geeksforgeeks.org/batch/skill-up-advanced-java/track/su-advanced-java-day8/problem/addition
+
+Ans-->
+
+  */
+
+class Addition
+  {
+  public static int add(int a,int b)
+    {
+      return a+b;
+    }
+      
+  }
