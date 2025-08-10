42ff8dcf7ba6470c243a4dd6965e4ddc7bccecdc
tusar-89 <sahut8941@gmail.com>
2025-08-10 09:10:24 -0900

    Create 1-->Reverse a String.java

---
+++ String/1-->Reverse a String.java
@@ -0,0 +1,30 @@
+/*
+Problem-->
+
+You are given a string s, and your task is to reverse the string.
+
+https://www.geeksforgeeks.org/problems/reverse-a-string/1?page=1&category=Strings&difficulty=Basic,Easy&sortBy=submissions
+
+Ans-->
+
+  */
+
+class Solution {
+    public static String reverseString(String s) {
+        // code here
+         char[] chars = s.toCharArray();
+        int left = 0, right = chars.length - 1;
+        
+        while (left < right) {
+            char temp = chars[left];
+            chars[left] = chars[right];
+            chars[right] = temp;
+            left++;
+            right--;
+        }
+        
+        return new String(chars);
+        /* 2nd method is following string builder method
+        -->return new StringBuilder(s).reverse().toString(); */
+    }
+}
