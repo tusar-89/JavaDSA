3fb33e64c4611a68b24e4d661c3148596db469d5
tusar-89 <sahut8941@gmail.com>
2025-08-11 05:31:05 -0900

    Create 2-->Palindrome String.java

---
+++ String/2-->Palindrome String.java
@@ -0,0 +1,29 @@
+/*
+Problem-->
+
+You are given a string s. Your task is to determine if the string is a palindrome.
+A string is considered a palindrome if it reads the same forwards and backwards.
+
+https://www.geeksforgeeks.org/problems/palindrome-string0817/1?page=1&category=Strings&sortBy=submissions
+
+Ans-->
+
+*/
+
+class Solution {
+    boolean isPalindrome(String s) {
+        // code here
+        int left=0;
+        int right=s.length()-1;
+        while(left<right)
+        {
+            if(s.charAt(left)!=s.charAt(right))
+            {
+               return false; 
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
