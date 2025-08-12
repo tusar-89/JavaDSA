ae8f1822984f0a117239557e3e25cd0d1c5a1167
tusar-89 <sahut8941@gmail.com>
2025-08-12 08:46:20 -0900

    Create 3-->Remove Spaces.java

---
+++ String/3-->Remove Spaces.java
@@ -0,0 +1,16 @@
+/*
+Problem-->
+
+  Given a string s. Your task is to remove spaces from it. 
+https://www.geeksforgeeks.org/problems/remove-spaces0128/1?page=2&category=Strings&difficulty=Basic,Easy&status=unsolved&sortBy=submissions
+  
+Ans-->
+    */
+
+class Solution {
+
+    String modify(String s) {
+        // your code here
+        return s.replaceAll(" ", "");
+    }
+}
