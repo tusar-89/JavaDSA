5b8612801f802cd29ccf1117173463bbcda13ed8
tusar-89 <sahut8941@gmail.com>
2025-08-15 08:22:44 -0900

    Create 6-->Convert String to LowerCase.java

---
+++ String/6-->Convert String to LowerCase.java
@@ -0,0 +1,16 @@
+/*
+Problem-->
+
+ Given a string s. The task is to convert string characters to lowercase.
+
+https://www.geeksforgeeks.org/problems/java-convert-string-to-lowercase2313/1?page=1&category=Strings&difficulty=Basic&status=unsolved&sortBy=submissions
+
+Ans-->
+  */
+
+class Solution {
+    static String toLower(String s) {
+        // code here
+        return s.toLowerCase();
+    }
+}
