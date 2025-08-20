b3770a60f7b73960beadfd1fe577172793fedc2f
tusar-89 <sahut8941@gmail.com>
2025-08-20 08:48:32 -0900

    Create 10-->Sort a String.java

---
+++ String/10-->Sort a String.java
@@ -0,0 +1,19 @@
+/*
+Problem-->
+  
+Given a string consisting of lowercase letters, arrange all its letters in ascending order. 
+
+  https://www.geeksforgeeks.org/problems/sort-a-string2943/1?page=1&category=Strings&difficulty=Basic&status=unsolved&sortBy=submissions
+
+Ans-->
+
+  */
+
+class Solution {
+    String sort(String s) {
+        // code here
+         char[] arr = s.toCharArray();   
+        Arrays.sort(arr);            
+        return new String(arr);     
+    }
+}
