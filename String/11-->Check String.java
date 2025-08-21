2874c53025571f526bec06985995c341b517a663
tusar-89 <sahut8941@gmail.com>
2025-08-21 08:26:34 -0900

    Create 11-->Check String.java

---
+++ String/11-->Check String.java
@@ -0,0 +1,25 @@
+/*
+Problem-->
+  
+Given a string, check if all its characters are the same or not.
+
+ https://www.geeksforgeeks.org/problems/check-string1818/1?page=1&category=Strings&difficulty=Basic&status=unsolved&sortBy=submissions
+
+Ans-->
+
+  */
+
+class Sol {
+    Boolean check(String s) {
+        // your code here
+        if (s.length() <= 1) return true;
+
+        char first = s.charAt(0);
+        for (int i = 1; i < s.length(); i++) {
+            if (s.charAt(i) != first) {
+                return false; // Found a different char
+            }
+        }
+        return true; // All same
+    }
+}
