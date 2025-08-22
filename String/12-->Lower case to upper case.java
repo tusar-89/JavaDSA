64732dd04e95fad3d224ee605117f0ab43e29913
tusar-89 <sahut8941@gmail.com>
2025-08-22 09:28:52 -0900

    Create 12-->Lower case to upper case.java

---
+++ String/12-->Lower case to upper case.java
@@ -0,0 +1,15 @@
+/*
+Problem-->
+
+Given a string str containing only lowercase letters, generate a string with the same letters, but in uppercase.
+
+  https://www.geeksforgeeks.org/problems/lower-case-to-upper-case3410/1?page=2&category=Strings&difficulty=Basic&status=unsolved&sortBy=submissions
+
+  Ans-->
+
+class Solution {
+    String to_upper(String str) {
+        // code here
+        return str.toUpperCase();
+    }
+}  
