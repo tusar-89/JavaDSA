4be8ca55c0581e7f2826f2181e5f10f6f3c3d3be
tusar-89 <sahut8941@gmail.com>
2025-08-26 09:07:27 -0900

    Create 16-->Remove vowels from string.java

---
+++ String/16-->Remove vowels from string.java
@@ -0,0 +1,19 @@
+/*
+Problem-->
+  
+Given a string s. Your task is to remove the vowels from the string.
+
+https://www.geeksforgeeks.org/problems/remove-vowels-from-string1446/1?page=5&category=Strings&status=unsolved&sortBy=submissions
+
+Ans-->
+
+  */
+
+// User function Template for Java
+
+class Solution {
+    String removeVowels(String s) {
+        // code here
+        return s.replaceAll("[aeiouAEIOU]","");
+    }
+}
