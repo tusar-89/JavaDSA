7ef34d03f0ec9d83076d69855f0c74c8f78446da
tusar-89 <sahut8941@gmail.com>
2025-09-11 08:54:05 -0900

    Create 108-->Print 1 To N Without Loop.java

---
+++ Dsa Practice/108-->Print 1 To N Without Loop.java
@@ -0,0 +1,22 @@
+/*
+Problem-->
+
+You are given an integer n. You have  to print all numbers from 1 to n.
+Note: You must use recursion only, and print all numbers from 1 to n in a single line, separated by spaces.
+
+https://www.geeksforgeeks.org/problems/print-1-to-n-without-using-loops-1587115620/1?page=3&status=unsolved&sortBy=submissions
+
+Ans-->
+
+*/
+
+class Solution {
+    public void printNos(int N) {
+        if (N > 0) 
+        {
+            printNos( N -1); 
+            System.out.print(N + " "); 
+        }
+        return;
+    }
+}
