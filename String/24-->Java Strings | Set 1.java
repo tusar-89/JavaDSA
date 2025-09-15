8652199dc8e6ec16b733fcd1e73b370833603701
tusar-89 <sahut8941@gmail.com>
2025-09-15 07:59:18 -0900

    Create 24-->Java Strings | Set 1.java

---
+++ String/24-->Java Strings | Set 1.java
@@ -0,0 +1,22 @@
+/*
+Problem-->
+
+Given two strings S1 and S2 as input. Your task is to concatenate two strings and then reverse the string. Finally print the reversed string.
+
+https://www.geeksforgeeks.org/problems/java-strings-set-15112/1?page=1&category=Strings&difficulty=Basic&status=unsolved&sortBy=submissions
+
+Ans-->
+  */
+class Solution {
+    static String conRevstr(String S1, String S2) {
+        // store the resulting string in final_S
+        String final_S = new String();
+        String concat_S = new String();
+        concat_S = S1 + S2;
+        for (int i = concat_S.length() - 1; i >= 0; i--){
+            final_S += concat_S.charAt(i);
+        }
+        // final resulting string
+        return final_S;
+    }
+}
