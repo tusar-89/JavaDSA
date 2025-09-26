b87908c456bd3dceade18b852ec8a74f7274db85
tusar-89 <sahut8941@gmail.com>
2025-09-26 09:00:03 -0900

    Create 112-->Median of an Array.java

---
+++ Dsa Practice/112-->Median of an Array.java
@@ -0,0 +1,26 @@
+/*
+
+Given an array arr[] of integers, calculate the median.
+
+https://www.geeksforgeeks.org/problems/find-the-median0527/1?page=1&difficulty=Basic&status=unsolved&sortBy=submissions
+
+Ans-->
+
+*/
+
+class Solution {
+    public double findMedian(int[] arr) {
+        // Code here.
+         Arrays.sort(arr);  // Step 1: Sort the array
+        int n = arr.length;
+        
+        // Step 2: Odd length → middle element
+        if (n % 2 != 0) {
+            return arr[n / 2];
+        } 
+        // Step 3: Even length → average of two middle elements
+        else {
+            return (arr[(n - 1) / 2] + arr[n / 2]) / 2.0;
+        }
+    }
+}
