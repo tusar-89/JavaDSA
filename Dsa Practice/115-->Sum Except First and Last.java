e58bf7afe4bd77b425448c34ea716be1b6e3789a
tusar-89 <sahut8941@gmail.com>
2025-09-30 09:28:40 -0900

    Create 115-->Sum Except First and Last.java

---
+++ Dsa Practice/115-->Sum Except First and Last.java
@@ -0,0 +1,24 @@
+/*
+Problem-->
+
+You are given an array arr of numbers. Return the sum of all the elements except the first and last elements.
+
+https://www.geeksforgeeks.org/problems/max-length-chain/1?page=1&difficulty=Basic&status=unsolved&sortBy=submissions
+
+Ans-->
+
+*/
+class Solution {
+    int sumExceptFirstLast(int[] arr) {
+        // your code here
+         if (arr.length <= 2) {
+            return 0;
+        }
+        
+        int sum = 0;
+        for (int i = 1; i < arr.length - 1; i++) {
+            sum += arr[i];
+        }
+        return sum;
+    }
+}
