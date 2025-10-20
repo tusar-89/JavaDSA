4a2360029fb50796bd260dc010e85233645fc30a
tusar-89 <sahut8941@gmail.com>
2025-10-20 05:24:19 -0900

    Add prefix average calculation method
    Implement a method to calculate the average of prefix sums for an array.

---
+++ Dsa Practice/127-->Average of Prefix.java
@@ -0,0 +1,26 @@
+/*
+
+Problem-->
+
+Given an array arr, find the average or mean of the prefix array at every index.
+
+https://www.geeksforgeeks.org/problems/average4856/1?page=4&category=Arrays&difficulty=Easy&status=unsolved&sortBy=submissions
+
+Ans-->
+
+*/
+
+class Solution {
+    // Prints average of a stream of numbers
+    public ArrayList<Integer> prefixAvg(ArrayList<Integer> arr) {
+        int n = arr.size();
+        ArrayList<Integer> ans = new ArrayList<>(
+            Collections.nCopies(n, 0)); 
+        int avg = 0; 
+        for (int i = 0; i < n; i++) {  
+            avg += arr.get(i);         
+            ans.set(i, avg / (i + 1)); 
+        }
+        return ans; 
+    }
+}
