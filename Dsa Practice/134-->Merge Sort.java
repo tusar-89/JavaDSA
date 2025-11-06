e541c26161fd44e792f25b221beeee49772d6d8c
tusar-89 <sahut8941@gmail.com>
2025-11-06 06:48:44 -0900

    Add problem statement for merge sort implementation
    Added a comment block explaining the merge sort problem and its solution.

---
+++ Dsa Practice/134-->Merge Sort.java
@@ -0,0 +1,69 @@
+/*
+
+Problem-->
+
+Given an array arr[], its starting position l and its ending position r. Sort the array using the merge sort algorithm.
+
+https://www.geeksforgeeks.org/problems/merge-sort/1?page=1&category=Sorting&sortBy=submissions
+
+Ans-->
+
+*/
+class Solution {
+    void mergeSort(int arr[], int l, int r) {
+        if (l < r) {
+            int m = l + (r - l) / 2;  // find the middle index
+
+            // Sort first and second halves
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            // Merge the sorted halves
+            merge(arr, l, m, r);
+        }
+    }
+
+    // Helper function to merge two sorted halves
+    void merge(int arr[], int l, int m, int r) {
+        int n1 = m - l + 1;
+        int n2 = r - m;
+
+        // Create temp arrays
+        int L[] = new int[n1];
+        int R[] = new int[n2];
+
+        // Copy data to temp arrays
+        for (int i = 0; i < n1; i++)
+            L[i] = arr[l + i];
+        for (int j = 0; j < n2; j++)
+            R[j] = arr[m + 1 + j];
+
+        // Merge the temp arrays
+        int i = 0, j = 0, k = l;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k] = L[i];
+                i++;
+            } else {
+                arr[k] = R[j];
+                j++;
+            }
+            k++;
+        }
+
+        // Copy remaining elements of L[]
+        while (i < n1) {
+            arr[k] = L[i];
+            i++;
+            k++;
+        }
+
+        // Copy remaining elements of R[]
+        while (j < n2) {
+            arr[k] = R[j];
+            j++;
+            k++;
+        }
+    }
+}
