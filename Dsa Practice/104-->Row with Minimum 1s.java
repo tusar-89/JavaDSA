e6d707f2e6f7ca8876dc19e5923d32147779bd60
tusar-89 <sahut8941@gmail.com>
2025-11-19 08:31:07 -0900

    Refine problem description for minimum 1s row
    Updated problem statement for clarity.

--- Dsa Practice/104-->Row with Minimum 1s.java
+++ Dsa Practice/104-->Row with Minimum 1s.java
@@ -1,5 +1,5 @@
 /*
-Problem-->
+Problem-->    
   
 Given a 2D binary matrix(1-based indexed) mat of dimensions nxm , determine the row that contains the minimum number of 1's.
 Note: The matrix contains only 1's and 0's. Also, if two or more rows contain the minimum number of 1's, the answer is the lowest of those indices.
