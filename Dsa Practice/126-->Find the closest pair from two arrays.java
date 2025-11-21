be80962c08bdc42ddaa750eeebc17b02f70d8655
tusar-89 <sahut8941@gmail.com>
2025-11-21 08:19:05 -0900

    Fix formatting of problem statement in Java file
    Updated problem statement formatting in the Java file.

--- Dsa Practice/126-->Find the closest pair from two arrays.java
+++ Dsa Practice/126-->Find the closest pair from two arrays.java
@@ -1,6 +1,6 @@
  /*
     
-Problem-->
+Problem-->  
 
 Given two sorted arrays arr and brr and a number x, find the pair whose sum is closest to x and the pair has an element from each array.
    In the case of multiple closest pairs return any one of them.
