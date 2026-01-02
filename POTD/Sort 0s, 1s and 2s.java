f43fce086520219b20ce2251ac39894fc3a98ee1
tusar-89 <sahut8941@gmail.com>
2026-01-01 20:47:45 -0900

    Add sorting algorithm for 0s, 1s, and 2s
    Implement Dutch National Flag algorithm to sort an array of 0s, 1s, and 2s in-place.

---
+++ POTD/Sort 0s, 1s and 2s.java
@@ -0,0 +1,41 @@
+/*
+Problem-->
+  
+Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
+Note: You need to solve this problem without utilizing the built-in sort function.
+
+  https://www.geeksforgeeks.org/problems/sort-an-array-of-0s-1s-and-2s4231/1 
+
+Ans-->
+
+  */
+
+class Solution {
+    public void sort012(int[] arr) {
+        // code here
+        int n=arr.length;
+        int low=0;
+        int mid=0;
+        int high=n-1;
+        while(mid<=high)
+        {
+            if(arr[mid]==0)
+            {
+                int temp=arr[low];
+                arr[low++]=arr[mid];
+                arr[mid++]=temp;
+            }
+            else if (arr[mid]==1)
+            {
+                mid++;
+            }
+            else
+            {
+                int temp=arr[mid];
+                arr[mid]=arr[high];
+                arr[high--]=temp;
+            }
+        }
+    }
+}
+
