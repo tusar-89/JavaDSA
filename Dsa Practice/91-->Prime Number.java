94961aee4d2f2063ecec4bccd62d008d732c2839
tusar-89 <sahut8941@gmail.com>
2026-02-19 07:55:34 -0900

    Update 91-->Prime Number.java
    Updated the prime number checking method with an optimal approach.

--- Dsa Practice/91-->Prime Number.java
+++ Dsa Practice/91-->Prime Number.java
@@ -1,4 +1,3 @@
-/*
 Problem-->
 
 Given a number n, determine whether it is a prime number or not.
@@ -6,8 +5,7 @@ Note: A prime number is a number greater than 1 that has no positive divisors ot
 
 https://www.geeksforgeeks.org/problems/prime-number2314/1?page=2&status=unsolved&sortBy=submissions
 
-Ans-->
-  */
+Ans-->Brute Force Approach
 
 class Solution {
     static boolean isPrime(int n) {
@@ -20,6 +18,21 @@ class Solution {
             }
         }
 
+        return true;
+    }
+}
+Optimal Approach-->
+
+ class Solution {
+    static boolean isPrime(int n) {
+        // code here
+        if (n <= 1) return false; // 0 and 1 are not prime numbers
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
