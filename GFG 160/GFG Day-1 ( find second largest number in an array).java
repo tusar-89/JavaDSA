57f04753afaa6cec8f1586f1a41e1fa3af58c3fd
tusar-89 <sahut8941@gmail.com>
2026-04-13 22:22:14 +0530

    Update GFG Day-1 ( find second largest number in an array).java
    Added problem statement link to the file.

--- GFG 160/GFG Day-1 ( find second largest number in an array).java
+++ GFG 160/GFG Day-1 ( find second largest number in an array).java
@@ -2,7 +2,7 @@
 
 https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/second-largest3735     
 
-<ANS><=> 
+<ANS><=>
  
 class Solution {
     public int getSecondLargest(int[] arr) {
